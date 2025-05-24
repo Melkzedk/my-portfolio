c44d457eb5dc37570f8f60de442d4c1b80baf48e
unknown <melkywafula29@gmail.com>
2025-05-25 01:59:39 +0300

    more  updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ const Footer = () => {
     <footer style={{ backgroundColor: '#212529', color: '#fff', padding: '20px 0' }}>
       <Container className="text-center">
         <div className="mb-3">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-light mx-3">
+          <a href="https://www.facebook.com/melkzedek.wafula" target="_blank" rel="noopener noreferrer" className="text-light mx-3">
             <FaFacebook size={24} />
           </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-light mx-3">
+          <a href="https://x.com/Melkzedekz?t=L0kiJlivKxSSLq8ItG4rdw&s=09" target="_blank" rel="noopener noreferrer" className="text-light mx-3">
             <FaTwitter size={24} />
           </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-light mx-3">
+          <a href="https://www.linkedin.com/in/melkzedek-wafula-4a8b46203" target="_blank" rel="noopener noreferrer" className="text-light mx-3">
             <FaLinkedin size={24} />
           </a>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="text-light mx-3">
+          <a href="https://github.com/Melkzedk" target="_blank" rel="noopener noreferrer" className="text-light mx-3">
             <FaGithub size={24} />
           </a>
         </div>
