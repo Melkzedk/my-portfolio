52516d384687e3afa37a46df96ccfe943d94dbd8
unknown <melkywafula29@gmail.com>
2025-05-25 02:16:06 +0300

    more  updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function MyNavbar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClick = () => {
+    setExpanded(false); // Close navbar after link click
+  };
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="shadow-sm">
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      sticky="top"
+      expanded={expanded}
+      onToggle={setExpanded}
+      className="shadow-sm"
+    >
       <Container>
         <Navbar.Brand href="#home" className="fw-bold text-info fs-4">
           My Portfolio
@@ -11,12 +25,12 @@ function MyNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto text-uppercase fw-semibold">
-            <Nav.Link href="#home" className="mx-2">Home</Nav.Link>
-            <Nav.Link href="#about" className="mx-2">About</Nav.Link>
-            <Nav.Link href="#skills" className="mx-2">Skills</Nav.Link>
-            <Nav.Link href="#projects" className="mx-2">Projects</Nav.Link>
-            <Nav.Link href="#references" className="mx-2">References</Nav.Link>
-            <Nav.Link href="#contact" className="mx-2">Contact</Nav.Link>
+            <Nav.Link href="#home" onClick={handleNavClick} className="mx-2">Home</Nav.Link>
+            <Nav.Link href="#about" onClick={handleNavClick} className="mx-2">About</Nav.Link>
+            <Nav.Link href="#skills" onClick={handleNavClick} className="mx-2">Skills</Nav.Link>
+            <Nav.Link href="#projects" onClick={handleNavClick} className="mx-2">Projects</Nav.Link>
+            <Nav.Link href="#references" onClick={handleNavClick} className="mx-2">References</Nav.Link>
+            <Nav.Link href="#contact" onClick={handleNavClick} className="mx-2">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
